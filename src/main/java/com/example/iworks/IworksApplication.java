81cfa3d4f8e5f7fd72959bc4cca9dfe1313d2d3a
SSAFY <dydwo2324@gmail.com>
2024-01-16 10:56:56 +0900

    init

---
+++ src/main/java/com/example/iworks/IworksApplication.java
@@ -0,0 +1,13 @@
+package com.example.iworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IworksApplication.class, args);
+	}
+
+}
