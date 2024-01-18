67534eba87d7ac794a985cee5a17836bf72c53d1
jiwon83 <gkswldnj0803@gmail.com>
2024-01-18 17:46:25 +0900

    add :  Add Relationship Between Board And Code

--- src/main/java/com/example/iworks/model/entity/Board.java
+++ src/main/java/com/example/iworks/model/entity/Board.java
@@ -9,40 +9,53 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
+@Table(name = "board")
+@IdClass(BoardId.class)
 public class Board {
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_category_code_id",
+            referencedColumnName = "code_id",
+            nullable = false, updatable = false, insertable = false
+    )
+    private Code boardCategoryCode; // 게시판 카테고리
+
+    @Id
+    @Column(name = "board_owner_id",
+            nullable = false, updatable = false, insertable = false )
+    private int boardOwnerId; // 게시판 주체 아이디
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "board_id", nullable = false)
-    private int id; // 글 아이디
+    private int boardId; // 글 아이디
 
     @Column(name = "board_creator_id", nullable = false)
-    private int creatorId; // 작성자 아이디
+    private int boardCreatorId; // 작성자 아이디
 
     @Column(name = "board_title", length = 30)
-    private String title; // 제목
+    private String boardTitle; // 제목
 
     @Column(name = "board_content", columnDefinition = "text")
-    private String content; // 내용
+    private String boardContent; // 내용
 
     @Column(name = "board_created_at")
-    private String createdAt; // 작성일시
+    private String boardCreatedAt; // 작성일시
 
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "board_updated_at")
-    private LocalDateTime updatedAt; // 수정일시
+    private LocalDateTime boardUpdatedAt; // 수정일시
 
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "board_deleted_at")
-    private LocalDateTime deletedAt; // 삭제일시
+    private LocalDateTime boardDeletedAt; // 삭제일시
 
     @Column(name = "board_is_deleted")
-    private boolean isDeleted; // 삭제여부
+    private boolean boardIsDeleted; // 삭제여부
 
     @Column(name = "board_modifier_id")
-    private Integer modifierId; // 수정자 아이디
+    private Integer boardModifierId; // 수정자 아이디
 
-    // 카테고리 고드 아이디
-    // 게시판 주체 아이디
 
 }
