61c92d92daa4e80ff61bd81a1677a835ad412191
jiwon83 <gkswldnj0803@gmail.com>
2024-01-19 12:35:48 +0900

    add: Add Two Way Mapping between Schedule and Meeting

--- src/main/java/com/example/iworks/model/entity/Meeting.java
+++ src/main/java/com/example/iworks/model/entity/Meeting.java
@@ -9,13 +9,15 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "meeting")
 @Getter
-@Setter
 public class Meeting {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "meeting_id", nullable = false)
-    private int id; // 회의방 아이디
+    private int meetingId; // 회의방 아이디
+
+    @OneToOne(mappedBy = "meeting", fetch = FetchType.LAZY)
+    private Schedule schedule; // 할 일 아이디(외래키)
 
     @Column(name = "meeting_date")
     private LocalDateTime meetingDate; // 회의 일시
--- src/main/java/com/example/iworks/model/entity/Schedule.java
+++ src/main/java/com/example/iworks/model/entity/Schedule.java
@@ -1,41 +1,75 @@
 package com.example.iworks.model.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "schedule")
+@Getter
+@IdClass(ScheduleId.class)
 public class Schedule {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "schedule_id", nullable = false)
-    private int id; // 할 일 아이디
+    private int scheduleId; // 할 일 아이디
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_category_code_id",
+            referencedColumnName = "code_id",
+            nullable = false, updatable = false, insertable = false
+    )
+    private Code scheduleCategoryCode; //할 일 카테코리 코드
+
+    @Id
+    @Column(name = "schedule_owner_id",
+            nullable = false, updatable = false, insertable = false
+    )
+    private int scheduleOwnerId; // 할 일 주체 아이디
+
     @Column(name = "schedule_title", nullable = false)
-    private String title; //할 일 이름
+    private String scheduleTitle; //할 일 이름
+
     @Column(name = "schedule_priority", nullable = false)
-    private String priority; //할 일 우선순위
+    private String schedulePriority; //할 일 우선순위 H: high, M:Medium, L:low
+
     @Column(name = "schedule_content")
-    private String content; //할 일 내용
+    private String scheduleContent; //할 일 내용
+
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "schedule_start_date", nullable = false)
-    private LocalDateTime startDate;
+    private LocalDateTime scheduleStartDate; //할 일의 시작일시
+
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "schedule_end_date", nullable = false)
-    private LocalDateTime endDate;
+    private LocalDateTime scheduleEndDate; //할 일의 종료일시
+
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "schedule_deadline")
-    private LocalDateTime deadline; //마감 일시
+    private LocalDateTime scheduleDeadline; //할 일의 마감일시
+
     @Column(name = "schedule_place")
-    private String place;
+    private String schedulePlace; //할 일의 장소
+
     @Column(name = "schedule_creator_id", nullable = false)
-    private int creatorId; // 등록자 아이디
+    private int scheduleCreatorId; // 등록자 아이디
+
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "schedule_created_at", nullable = false)
-    private LocalDateTime createdAt; //등록 일시
+    private LocalDateTime scheduleCreatedAt; //등록 일시
+
     @Column(name = "schedule_modifier_id")
-    private int modifierId;
+    private int scheduleModifierId; // 수정한 사람의 아이디
+
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "schedule_modified_at")
-    private LocalDateTime modifiedAt;
+    private LocalDateTime scheduleModifiedAt; //할 일의 수정일시
+
+    @OneToOne
+    @JoinColumn(name = "meeting_id")
+    private Meeting meeting;
+
 
 }
