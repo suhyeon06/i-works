6c086f3b048b06aef83692547ccbcd8468daf8d9
jiwon83 <gkswldnj0803@gmail.com>
2024-01-25 19:16:48 +0900

    feat: Add ScheduleAssign Entity - Add function in Schedule Domain: can select several assignee when creating a schedule.

---
+++ I-works_server/src/main/java/com/example/iworks/schedule/model/entitiy/ScheduleAssign.java
@@ -0,0 +1,38 @@
+package com.example.iworks.schedule.model.entitiy;
+
+import com.example.iworks.global.model.entity.Code;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Table(name = "schedule_assign")
+@Getter
+public class ScheduleAssign {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "schedule_assign_id", nullable = false)
+    private int scheduleAssignId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
+
+    public void setSchedule(Schedule schedule){
+        if (this.schedule != null){ //이미 연관 관계가 있었다면
+            this.schedule.getScheduleAssigns().remove(this);
+        }
+        this.schedule = schedule;
+        schedule.getScheduleAssigns().add(this);
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_assignee_category_id", referencedColumnName = "code_id")
+    private Code scheduleAssigneeCategoryId; // 할일 배정자 카테고리 아이디
+
+    @Column(name = "schedule_assignee_id", nullable = false, insertable = false, updatable = false)
+    private int scheduleAssigneeId; //유저아이디 or 그룹아이디 or 부서아이디  or 전체
+
+}
+
+
