086c2ac5ee76cfe1e9e6376b4ab9d7ab49a1c8eb
DevYJKwon <dydwo2324@gmail.com>
2024-01-26 17:13:07 +0900

    add: add jwt redis validation

--- I-works_server/src/main/java/com/example/iworks/global/config/jwt/JwtProvider.java
+++ I-works_server/src/main/java/com/example/iworks/global/config/jwt/JwtProvider.java
@@ -2,63 +2,87 @@ package com.example.iworks.global.config.jwt;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Component
+@RequiredArgsConstructor
 public class JwtProvider {
     private static final String SECRET_KEY = "sd#$%#$fnsWRTWRTsdfnsdSDFSDfnds##wr412";
+    private final RedisTemplate<String, String> redisTemplate;
+    @Value("${jwt.accessExpTime}")
+    long accessExpTime;
 
-    public String createAccessToken(String eid, List<String> role){
+    @Value("${jwt.refreshExpTime}")
+    long refreshExpTime;
+
+    public String createAccessToken(String eid, List<String> role) {
         String accessToken = JWT.create()
-                .withExpiresAt(new Date(System.currentTimeMillis() + (60000 * 10))) // 토큰의 만료시간 10분
-                .withClaim("type","access")
+                .withExpiresAt(new Date(System.currentTimeMillis() + accessExpTime)) // 토큰의 만료시간 10분
+                .withClaim("type", "access")
                 .withClaim("eid", eid)
                 .withClaim("role", role)
                 .sign(Algorithm.HMAC512(SECRET_KEY));
         return accessToken;
     }
 
-    public String reCreateAccessToken(String refreshToken){
+    public String reCreateAccessToken(String refreshToken) {
         String eid = JWT.require(Algorithm.HMAC512(SECRET_KEY)).build().verify(refreshToken).getClaim("eid").asString();
         List<String> role = JWT.require(Algorithm.HMAC512(SECRET_KEY)).build().verify(refreshToken).getClaim("role").asList(String.class);
-        return createAccessToken(eid,role);
+        return createAccessToken(eid, role);
     }
 
-    public String createRefreshToken(String eid,List<String> role){
+    public String createRefreshToken(String eid, List<String> role) {
         String refreshToken = JWT.create()
-                .withClaim("type","refresh")
+                .withClaim("type", "refresh")
                 .withClaim("eid", eid)
                 .withClaim("role", role)
-                .withExpiresAt(new Date(System.currentTimeMillis() + 86400000)) // 86400000 하루
+                .withExpiresAt(new Date(System.currentTimeMillis() + refreshExpTime)) // 86400000 하루
                 .sign(Algorithm.HMAC512(SECRET_KEY));
+
+        redisTemplate.opsForValue().set(
+                refreshToken, //key
+                eid, //value
+                refreshExpTime,
+                TimeUnit.MILLISECONDS
+        );
+
         return refreshToken;
     }
 
-    public Boolean validateAccessToken(String accessToken){
+    public Boolean validateAccessToken(String accessToken) {
         System.out.println("val access");
         String type = JWT.require(Algorithm.HMAC512(SECRET_KEY)).build().verify(accessToken).getClaim("type").asString();
         String eid = JWT.require(Algorithm.HMAC512(SECRET_KEY)).build().verify(accessToken).getClaim("eid").asString();
         return "access".equals(type) && eid != null;
     }
 
-    public Boolean validateRefreshToken(String refreshToken){
+    public Boolean validateRefreshToken(String refreshToken) {
         String type = JWT.require(Algorithm.HMAC512(SECRET_KEY)).build().verify(refreshToken).getClaim("type").asString();
-        if(type.equals("refresh")){
+        if (type.equals("refresh")) {
             System.out.println("success");
-            return JWT.require(Algorithm.HMAC512(SECRET_KEY)).build().verify(refreshToken).getExpiresAt().after(new Date());
+            ValueOperations<String, String> stringValueOperations = redisTemplate.opsForValue();
+            String redisValue = stringValueOperations.get(refreshToken);
+            if (redisValue != null) {
+                return JWT.require(Algorithm.HMAC512(SECRET_KEY)).build().verify(refreshToken).getExpiresAt().after(new Date());
+            }
         }
         System.out.println("failed");
         return false;
     }
 
-    public String getUserEid(String jwt){
+    public String getUserEid(String jwt) {
         return JWT.require(Algorithm.HMAC512(SECRET_KEY)).build().verify(jwt).getClaim("eid").asString();
     }
 
-    public List<String> getUserRole(String jwt){
+    public List<String> getUserRole(String jwt) {
         return JWT.require(Algorithm.HMAC512(SECRET_KEY)).build().verify(jwt).getClaim("role").asList(String.class);
     }
 }
