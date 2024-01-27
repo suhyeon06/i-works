a7bcc4c2d9ce94999aa0091855c34e67dd55eb64
Kim Ji Yong <jykim00324@gmail.com>
2024-01-27 22:20:06 +0900

    fix: Change store/.js to .ts

--- I-works_client/src/main.tsx
+++ I-works_client/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import store from './store/index.js'
+import store from './store/index'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
