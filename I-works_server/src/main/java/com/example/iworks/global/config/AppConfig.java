ceead24338a212833a376fda28deed45910c3acd
jiwon83 <gkswldnj0803@gmail.com>
2024-01-31 13:24:46 +0900

    chore : Modify config, build, properties

--- I-works_server/src/main/java/com/example/iworks/global/config/AppConfig.java
+++ I-works_server/src/main/java/com/example/iworks/global/config/AppConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories(basePackages = "com.example.iworks.domain.*.repository")
+@EnableJpaRepositories(basePackages = "com.example.iworks.*")
 public class AppConfig {
 }
