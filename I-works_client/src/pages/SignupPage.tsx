45f62de75fcb9fc3bb546143e3cd50616fac69c0
Kim Ji Yong <jykim00324@gmail.com>
2024-01-31 09:45:03 +0900

    fix: Change signup formdata to json

--- I-works_client/src/pages/SignupPage.tsx
+++ I-works_client/src/pages/SignupPage.tsx
@@ -16,15 +16,15 @@ export interface SignupRef {
 
 const API_URL = 'https://https://suhyeon.site/api/user/join'
 
-// function formDataToJson(formData: FormData): string {
-//   const json: Record<string, string> = {};
+function formDataToJson(formData: FormData): string {
+  const json: Record<string, string> = {};
 
-//   formData.forEach((value, key) => {
-//     json[key] = value.toString();
-//   });
+  formData.forEach((value, key) => {
+    json[key] = value.toString();
+  });
 
-//   return JSON.stringify(json);
-// }
+  return JSON.stringify(json);
+}
 
 const Signup = forwardRef<SignupRef>(function Signup(_props, ref) {
   const dialog = useRef<HTMLDialogElement>(null)
@@ -44,13 +44,14 @@ const Signup = forwardRef<SignupRef>(function Signup(_props, ref) {
     event.preventDefault()
 
     const signupFormData = new FormData(event.currentTarget)
+    const signupJsonData = formDataToJson(signupFormData)
 
     axios
-      .post(API_URL, signupFormData)
+      .post(API_URL, signupJsonData)
       .then((response) => {
         console.log('res', response)
         console.log('res.data', response.data)
-        
+
         alert('구성원이 추가 되었습니다.')
         formRef.current?.reset()        
       })
