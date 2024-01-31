61403f52bb7791a3fdc30c382fa4b0c86fc25423
DevYJKwon <dydwo2324@gmail.com>
2024-01-31 12:46:43 +0900

    fix: open cors condition put

--- I-works_server/src/main/java/com/example/iworks/global/filter/CustomCorsFilter.java
+++ I-works_server/src/main/java/com/example/iworks/global/filter/CustomCorsFilter.java
@@ -22,7 +22,7 @@ public class CustomCorsFilter implements Filter {
 
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods","GET,POST");
+        response.setHeader("Access-Control-Allow-Methods","GET,POST,PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept, Authorization");
