305cecc3784eb0df02398231d2e47ca96e4fbcf9
DevYJKwon <dydwo2324@gmail.com>
2024-01-31 12:22:22 +0900

    fix: change cors simple request to credential

--- I-works_server/src/main/java/com/example/iworks/global/filter/CustomCorsFilter.java
+++ I-works_server/src/main/java/com/example/iworks/global/filter/CustomCorsFilter.java
@@ -20,9 +20,9 @@ public class CustomCorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods","*");
+        response.setHeader("Access-Control-Allow-Methods","GET,POST");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept, Authorization");
