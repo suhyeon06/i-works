d5258198e593c4f5e7d16a2aef1fd6ad3e915072
Kim Ji Yong <jykim00324@gmail.com>
2024-01-31 10:45:07 +0900

    fix: Signup change API_URL

--- I-works_client/src/pages/SignupPage.tsx
+++ I-works_client/src/pages/SignupPage.tsx
@@ -7,36 +7,28 @@ import {
 } from 'react'
 import { createPortal } from 'react-dom'
 import { Form } from 'react-router-dom'
-import axios, { AxiosResponse } from 'axios'
+import axios from 'axios'
 import { TextInput, Button, Radio, Label } from 'flowbite-react'
 
 export interface SignupRef {
   open: () => void
 }
 
-interface SignupResponse {
-  result: string
-  data: {
-    data: string
-    message: string
-  }
-}
+const API_URL = 'https://suhyeon.site/api/user/join'
 
-const API_URL = 'https://https://suhyeon.site/api/user/join'
+// function formDataToJson(formData: FormData): string {
+//   const json: Record<string, string> = {};
 
-function formDataToJson(formData: FormData): string {
-  const json: Record<string, string> = {};
+//   formData.forEach((value, key) => {
+//     json[key] = value.toString();
+//   });
 
-  formData.forEach((value, key) => {
-    json[key] = value.toString();
-  });
-
-  return JSON.stringify(json);
-}
+//   return JSON.stringify(json);
+// }
 
 const Signup = forwardRef<SignupRef>(function Signup(_props, ref) {
   const dialog = useRef<HTMLDialogElement>(null)
-  const formRef = useRef<HTMLFormElement>(null)
+  const formRef = useRef<HTMLFormElement>(null);
 
   useImperativeHandle(
     ref,
@@ -48,22 +40,20 @@ const Signup = forwardRef<SignupRef>(function Signup(_props, ref) {
     [],
   )
 
-  async function handleSignUp(event: FormEvent<HTMLFormElement>) {
+  function handleSignUp(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
 
     const signupFormData = new FormData(event.currentTarget)
-    const signupJsonData = formDataToJson(signupFormData)
 
-    try {
-      const response: AxiosResponse<SignupResponse> = await axios.post(
-        API_URL,
-        signupJsonData,
-      )
-      alert(response.data.data.message)
-      formRef.current?.reset()
-    } catch (error: any) {
-      alert(error.data.message)
-    }
+    axios
+      .post(API_URL, signupFormData)
+      .then((response) => {
+        alert(response.data.data.message)
+        formRef.current?.reset()        
+      })
+      .catch((error) => {
+        alert(error.data.message)
+      })
   }
 
   return createPortal(
@@ -72,11 +62,7 @@ const Signup = forwardRef<SignupRef>(function Signup(_props, ref) {
       ref={dialog as RefObject<HTMLDialogElement>}
     >
       <h1 className="text-3xl text-center mb-10">구성원 추가</h1>
-      <Form
-        ref={formRef}
-        className="flex flex-col gap-4"
-        onSubmit={handleSignUp}
-      >
+      <Form ref={formRef} className="flex flex-col gap-4" onSubmit={handleSignUp}>
         <div>
           <Label className="text-lg">사번</Label>
           <TextInput type="text" name="userEid" required />
@@ -95,7 +81,7 @@ const Signup = forwardRef<SignupRef>(function Signup(_props, ref) {
         </div>
         <div>
           <Label className="text-lg">전화번호</Label>
-          <TextInput type="text" name="userTel" required />
+          <TextInput type="text" name="userTel" required/>
         </div>
         <div>
           <Label className="text-lg">주소</Label>
