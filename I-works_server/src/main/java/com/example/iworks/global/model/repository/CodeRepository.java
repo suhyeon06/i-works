864260b7509be51fb090ada5bf3d621bbbe27f8a
park95gr <pkr951113@ajou.ac.kr>
2024-02-01 14:05:50 +0900

    feat: Add Code Repository

---
+++ I-works_server/src/main/java/com/example/iworks/global/model/repository/CodeRepository.java
@@ -0,0 +1,10 @@
+package com.example.iworks.global.model.repository;
+
+import com.example.iworks.global.model.entity.Code;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CodeRepository extends JpaRepository<Code, Integer> {
+
+    Code findCodeByCodeName(String name);
+
+}
