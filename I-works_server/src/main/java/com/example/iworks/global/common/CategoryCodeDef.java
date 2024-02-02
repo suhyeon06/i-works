3db9b77f556b92f6d587d39a3d716a1680465e61
jiwon83 <gkswldnj0803@gmail.com>
2024-02-03 00:44:16 +0900

    refactor : Extracting Common Final Variable About Category Code

---
+++ I-works_server/src/main/java/com/example/iworks/global/common/CategoryCodeDef.java
@@ -0,0 +1,9 @@
+package com.example.iworks.global.common;
+
+public class CategoryCodeDef {
+
+    public final static int CATEGORY_USER_CODE_ID = 1;
+    public final static int CATEGORY_DEPARTMENT_CODE_ID = 2;
+    public final static int CATEGORY_TEAM_CODE_ID = 3;
+
+}
