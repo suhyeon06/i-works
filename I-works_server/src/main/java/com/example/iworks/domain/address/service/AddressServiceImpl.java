eb7ef7ef9dd5d37f079c60d594c155745f4cfd55
DevYJKwon <dydwo2324@gmail.com>
2024-02-05 15:29:12 +0900

    fix : change allAddress response type list to array

--- I-works_server/src/main/java/com/example/iworks/domain/address/service/AddressServiceImpl.java
+++ I-works_server/src/main/java/com/example/iworks/domain/address/service/AddressServiceImpl.java
@@ -2,7 +2,6 @@ package com.example.iworks.domain.address.service;
 
 import com.example.iworks.domain.address.dto.AddressDto;
 import com.example.iworks.domain.address.dto.AddressOrgChartResonseDto;
-import com.example.iworks.domain.address.dto.AddressResponseDto;
 import com.example.iworks.domain.address.respository.AddressRepository;
 import com.example.iworks.domain.department.domain.Department;
 import com.example.iworks.domain.department.repository.DepartmentRepository;
@@ -12,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
@@ -32,11 +30,7 @@ public class AddressServiceImpl implements AddressService {
         if(result.isEmpty()){
             return response.handleFail("조회 내용 없음.");
         }
-        Map<Integer, AddressResponseDto> data = new HashMap<>();
-        for(int i=0; i< result.size(); i++){
-            data.put(i+1,new AddressResponseDto(result.get(i)));
-        }
-        return response.handleSuccess(data);
+        return response.handleSuccess(result);
     }
 
     @Override
