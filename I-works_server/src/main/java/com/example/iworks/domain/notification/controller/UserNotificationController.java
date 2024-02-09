39ad2572e13175c6236f18c7453dde27d9ef2da8
jiwon83 <gkswldnj0803@gmail.com>
2024-02-09 19:37:22 +0900

    add : Modify UserNotificationController Layer

--- I-works_server/src/main/java/com/example/iworks/domain/notification/controller/UserNotificationController.java
+++ I-works_server/src/main/java/com/example/iworks/domain/notification/controller/UserNotificationController.java
@@ -1,11 +1,10 @@
 package com.example.iworks.domain.notification.controller;
 
 
-import com.example.iworks.domain.notification.dto.notification.NotificationCreateRequestDto;
-import com.example.iworks.domain.notification.service.NotificationService;
+import com.example.iworks.domain.notification.dto.usernotification.request.UserNotificationCreateRequestDto;
+import com.example.iworks.domain.notification.service.UserNotificationService;
 import com.example.iworks.global.model.Response;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,21 +12,21 @@ import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/notification")
-public class NotificationController {
+@RequestMapping("/api/user-notification")
+public class UserNotificationController {
     
-    private final NotificationService notificationService;
+    private final UserNotificationService userNotificationService;
     private final Response response;
 
     @PostMapping("/")
-    public ResponseEntity<Map<String, Object>> createNotification(@RequestBody NotificationCreateRequestDto notificationCreateRequestDto) {
-        notificationService.createNotification(notificationCreateRequestDto);
+    public ResponseEntity<Map<String, Object>> createNotification(@RequestBody UserNotificationCreateRequestDto userNotificationCreateRequestDto) {
+        userNotificationService.createNotification(userNotificationCreateRequestDto);
         return response.handleSuccess("알림 생성 완료");
     }
 
     @GetMapping("/{notificationId}/delete")
     public ResponseEntity<Map<String, Object>> deleteNotification(@PathVariable("notificationId") int notificationId) {
-        notificationService.deleteNotification(notificationId);
+        userNotificationService.deleteNotification(notificationId);
         return response.handleSuccess("알림 삭제 완료");
     }
 
