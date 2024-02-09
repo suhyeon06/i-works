9e95cbf08f65c6f216e50ff58fd23d9d92f85b74
jiwon83 <gkswldnj0803@gmail.com>
2024-02-10 00:45:25 +0900

    add : UserNotification API - Add findUserNotificationsByUserId Service & Controller

--- I-works_server/src/main/java/com/example/iworks/domain/notification/service/UserNotificationService.java
+++ I-works_server/src/main/java/com/example/iworks/domain/notification/service/UserNotificationService.java
@@ -1,8 +1,15 @@
 package com.example.iworks.domain.notification.service;
 
+import com.example.iworks.domain.notification.dto.usernotification.response.UserNotificationGetAllByUserResponseDto;
 import com.example.iworks.domain.notification.dto.usernotification.request.UserNotificationCreateRequestDto;
 
+import java.util.List;
+
 public interface UserNotificationService {
+
     void createUserNotification(UserNotificationCreateRequestDto createRequestDto);
+
     void deleteUserNotification(int userNotificationId);
+
+    List<UserNotificationGetAllByUserResponseDto> getAllUserNotificationsByUserId(int userId);
 }
