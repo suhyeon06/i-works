32b3dd6683f1a0cde1475bdac8e8cd133a78e24e
DevYJKwon <DevYJKwon>
2024-02-10 10:21:30 +0900

    add : add LeaderApiController

---
+++ I-works_server/src/main/java/com/example/iworks/domain/leader/controller/LeaderApiController.java
@@ -0,0 +1,11 @@
+package com.example.iworks.domain.leader.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/leader")
+@RequiredArgsConstructor
+public class LeaderApiController {
+}
