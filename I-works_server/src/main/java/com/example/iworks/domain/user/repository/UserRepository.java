77925452e9146de34118c1da5bc75b44b0bf4298
jiwon83 <gkswldnj0803@gmail.com>
2024-02-13 00:52:33 +0900

    feat : Add method Finding Users ByDepartment Id and Team Id in UserRepository

--- I-works_server/src/main/java/com/example/iworks/domain/user/repository/UserRepository.java
+++ I-works_server/src/main/java/com/example/iworks/domain/user/repository/UserRepository.java
@@ -1,15 +1,27 @@
 package com.example.iworks.domain.user.repository;
 
 import com.example.iworks.domain.user.domain.User;
+import com.example.iworks.domain.user.repository.custom.UserSearchRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
+public interface UserRepository extends JpaRepository<User,Integer>, UserSearchRepository {
 
     User findByUserEid(String userEid);
 
     User findByUserId(int userId);
 
-    public User findByUserEmail(String userEmail);
+    User findByUserEmail(String userEmail);
+
+    @Query("select u from User u join u.userDepartment d where d.departmentId = :departmentId")
+    List<User> findUsersByDepartmentId(@Param("departmentId") Integer departmentId);
+
+    @Query("select u from User u join TeamUser tu where tu.teamUserTeam.teamId = :teamId")
+    List<User> findUsersByTeamId(@Param("teamId") Integer teamId);
 }
