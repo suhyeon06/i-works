e62daf3152fe5a529a840efd20188fe11d7d7730
jiwon83 <gkswldnj0803@gmail.com>
2024-02-13 18:18:13 +0900

    add : Add Nullable Constraints in Entity Column(Department.name, Team.name)

--- I-works_server/src/main/java/com/example/iworks/domain/team/domain/Team.java
+++ I-works_server/src/main/java/com/example/iworks/domain/team/domain/Team.java
@@ -23,7 +23,7 @@ public class Team {
     @Column(name = "team_id")
     private int teamId; // 그룹 아이디
 
-    @Column(name = "team_name", length = 20, nullable = false)
+    @Column(name = "team_name", length = 20, nullable = false, unique = true)
     private String teamName; // 그룹명
 
     @Column(name = "team_leader", nullable = false)
