d4f43a98331dc31d5a1ad134b981d0a2f6357527
DevYJKwon <DevYJKwon>
2024-02-14 22:10:34 +0900

    add: schedule edit

--- I-works_client/src/pages/admins/AdminSchedulesUpdate.tsx
+++ I-works_client/src/pages/admins/AdminSchedulesUpdate.tsx
@@ -1,46 +1,70 @@
 import axios from 'axios'
 import { ChangeEvent, FormEvent, useEffect, useState } from 'react'
 import { useNavigate, useParams, Form } from 'react-router-dom'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
-import ScheduleModal from '../../components/ScheduleModal'
-import ReactQuill from "react-quill"
-import "react-quill/dist/quill.snow.css"
+import 'react-quill/dist/quill.snow.css'
 import { Button } from 'flowbite-react'
 
-const AdminSchedulesUpdate = () => {
-  const { scheduleId = '' } = useParams<{ scheduleId: string }>()
+interface ScheduleEditForm {
+  scheduleDivisionCodeId: number
+  scheduleTitle: string
+  schedulePriority: string
+  scheduleContent: string
+  scheduleStartDate: Date
+  scheduleEndDate: Date
+  meetingDate: Date
+  schedulePlace: string
+  meetingCode: string
+}
 
+const AdminSchedulesUpdate: React.FC = () => {
+  const { scheduleId = '' } = useParams<{ scheduleId: string }>()
+  const [formState, setFormState] = useState<ScheduleEditForm>()
   const navigate = useNavigate()
   const backToIndex = () => {
     navigate('/admin/schedules/')
   }
 
-  const [scheduleTitle, setScheduleTitle] = useState<string>('')
-  const [scheduleContent, setScheduleContent] = useState<string>('')
-  const [scheduleOwnerId, setScheduleOwnerId] = useState<string>('')
-  const [scheduleCategoryCodeId, setCategoryCodeId] = useState<string>('')
-  // const [scheduleModifierId, setScheduleModifierId] = useState<number>()
-
-  const onTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setScheduleTitle(event.target.value)
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target
+    console.log(name)
+    setFormState({
+      ...formState,
+      [name]: value,
+    })
   }
 
-  const onContentChange = (content: string) => {
-    setScheduleContent(content)
+  const handleDateChange = (date: Date, name: string) => {
+    setFormState({
+      ...formState,
+      [name]: date,
+    })
   }
 
   useEffect(() => {
     async function getScheduleDetail(scheduleId: string) {
       try {
-        const res = await axios.get(`https://suhyeon.site/api/schedule/${scheduleId}`)
+        const res = await axios.get(
+          `https://suhyeon.site/api/schedule/${scheduleId}`,
+        )
         const scheduleDetailData = res.data.data
-
-        setScheduleTitle(scheduleDetailData.scheduleTitle)
-        setScheduleContent(scheduleDetailData.scheduleContent)
-        setScheduleOwnerId(scheduleDetailData.scheduleOwnerId)
-        setCategoryCodeId(scheduleDetailData.scheduleCategoryId)
-      }
-      catch (err) {
+        const initData: ScheduleEditForm = {
+          //scheduleDivisionCodeId: scheduleDetailData.scheduleDivisionCodeId,
+          scheduleDivisionCodeId: 1,
+          scheduleTitle: scheduleDetailData.scheduleTitle,
+          schedulePriority: scheduleDetailData.schedulePriority,
+          scheduleContent: scheduleDetailData.scheduleContent,
+          scheduleStartDate: scheduleDetailData.scheduleStartDate,
+          scheduleEndDate: scheduleDetailData.scheduleEndDate,
+          schedulePlace: scheduleDetailData.schedulePlace,
+          //meetingCode: scheduleDetailData.meetingCode,
+          meetingCode: 'test',
+          meetingDate: scheduleDetailData.meetingDate,
+        }
+        setFormState(initData)
+      } catch (err) {
         console.log(err)
       }
     }
@@ -50,18 +74,9 @@ const AdminSchedulesUpdate = () => {
 
   function handleUpdate(event: FormEvent) {
     event.preventDefault()
-    const plainTextContent = (scheduleContent || '').replace(/<[^>]+>/g, '');
-    const updateSchedule = {
-      "scheduleTitle": scheduleTitle,
-      "scheduleContent": plainTextContent,
-      "scheduleCreatorId": '1',
-      "scheduleIsDeleted": '0',
-      "scheduleCategoryCodeId": scheduleCategoryCodeId,
-      "scheduleOwnerId": scheduleOwnerId,
-    }
-
+    console.log(formState)
     axios
-      .put(`https://suhyeon.site/api/schedule/update/${scheduleId}`, updateSchedule)
+      .post(`https://suhyeon.site/api/schedule/${scheduleId}/update`, formState)
       .then(() => {
         alert('수정되었습니다.')
         navigate('/admin/schedules/')
@@ -71,55 +86,90 @@ const AdminSchedulesUpdate = () => {
       })
   }
 
-  // 모달창 구현
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [selectedSchedule, setselectedSchedule] = useState<string | null>(null);
-
-  const openModal = () => {
-    setModalIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalIsOpen(false);
-  };
-
-  const handleSelectSchedule = (scheduleName: string, scheduleOwnerId: string, scheduleCategoryCodeId: string) => {
-    setselectedSchedule(scheduleName);
-    setScheduleOwnerId(scheduleOwnerId)
-    setCategoryCodeId(scheduleCategoryCodeId)
-  };
-
   return (
     <div>
       <div className="h-10 mb-2 border-b-2 text-xl">
         <h1>할 일 수정</h1>
       </div>
       <Form className="flex flex-col" onSubmit={handleUpdate}>
-        <div className="flex items-center">
-          <p className="ml-2">{selectedSchedule}</p>
-          <ScheduleModal
-            show={modalIsOpen}
-            onClose={closeModal}
-            onSelect={handleSelectSchedule}
-          />
-        </div>
         <div className="flex items-center my-2">
-          <label className="mr-14" htmlFor="title">제목 : </label>
-          <input onChange={onTitleChange} className="h-8 w-3/4" type="text" name="scheduleTitle" value={scheduleTitle} id="title" required />
+          <label className="mr-14" htmlFor="title">
+            제목 :{' '}
+          </label>
+          <input
+            onChange={handleInputChange}
+            className="h-8 w-3/4"
+            type="text"
+            name="scheduleTitle"
+            value={formState?.scheduleTitle}
+            id="title"
+            required
+          />
         </div>
 
         <div className="flex items-center my-2">
-          <label className="mr-14" htmlFor="content">내용 : </label>
-          <input onChange={onTitleChange} className="h-8 w-3/4" type="text" name="scheduleContent" value={scheduleTitle} id="content" required />
+          <label className="mr-14" htmlFor="content">
+            내용 :{' '}
+          </label>
+          <input
+            onChange={handleInputChange}
+            className="h-8 w-3/4"
+            type="text"
+            name="scheduleContent"
+            value={formState?.scheduleContent}
+            id="content"
+            required
+          />
+        </div>
+
+        <div className="flex items-center my-2">
+          <label className="mr-14" htmlFor="content">
+            장소 :{' '}
+          </label>
+          <input
+            onChange={handleInputChange}
+            className="h-8 w-3/4"
+            type="text"
+            name="schedulePlace"
+            value={formState?.schedulePlace}
+            id="content"
+            required
+          />
+        </div>
+
+        <div className="flex items-center my-2">
+          <label className="mr-14" htmlFor="content">
+            시작 시간 :{' '}
+          </label>
+          <DatePicker
+            showIcon
+            dateFormat="yyyy.MM.dd" // 날짜 형태
+            shouldCloseOnSelect // 날짜를 선택하면 datepicker가 자동으로 닫힘
+            selected={formState?.scheduleStartDate}
+            onChange={(date) => handleDateChange(date!, 'scheduleStartDate')}
+          />
+        </div>
+        <div className="flex items-center my-2">
+          <label className="mr-14" htmlFor="content">
+            마감 시간 :{' '}
+          </label>
+          <DatePicker
+            showIcon
+            dateFormat="yyyy.MM.dd" // 날짜 형태
+            shouldCloseOnSelect // 날짜를 선택하면 datepicker가 자동으로 닫힘
+            selected={formState?.scheduleEndDate}
+            onChange={(date) => handleDateChange(date!, 'scheduleEndDate')}
+          />
         </div>
         <br />
-        <div className='flex justify-end mt-10 '>
-          <Button className='bg-mainGreen mr-4' type='submit'>수정</Button>
+        <div className="flex justify-end mt-10 ">
+          <Button className="bg-mainGreen mr-4" type="submit">
+            수정
+          </Button>
           <Button onClick={backToIndex}>취소</Button>
         </div>
       </Form>
     </div>
   )
 }
-
 export default AdminSchedulesUpdate
