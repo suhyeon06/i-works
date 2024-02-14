00ffb2722687ed08c6b90222655c3849a3088f94
jiwon83 <gkswldnj0803@gmail.com>
2024-02-15 05:09:00 +0900

    refactor : Refactor ScheduleRepositoryImpl

--- I-works_server/src/main/java/com/example/iworks/domain/schedule/repository/schedule/ScheduleRepositoryImpl.java
+++ I-works_server/src/main/java/com/example/iworks/domain/schedule/repository/schedule/ScheduleRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.example.iworks.domain.schedule.repository.schedule;
 
 import com.example.iworks.domain.schedule.dto.schedule.response.ScheduleResponseDto;
 import com.example.iworks.domain.schedule.repository.schedule.custom.ScheduleSearchRepository;
+import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -19,9 +20,8 @@ public class ScheduleRepositoryImpl implements ScheduleSearchRepository {
     public List<ScheduleResponseDto> findByKeyword(String keyword) {
         return queryFactory
                 .selectFrom(schedule)
-                .where(containsTitle(keyword)
-                        .or(containsCreatorName(keyword))
-                        .and(schedule.scheduleIsDeleted.isFalse())
+                .where(containsKeyword(keyword)
+                        .and(notDeleted())
                 )
                 .fetch()
                 .stream()
@@ -29,10 +29,14 @@ public class ScheduleRepositoryImpl implements ScheduleSearchRepository {
                 .collect(toList());
     }
 
-    private BooleanExpression containsTitle(String keyword){
-        return schedule.scheduleTitle.like("%"+keyword+"%");
+    private BooleanExpression notDeleted() {
+        return schedule.scheduleIsDeleted.isFalse();
     }
-    private BooleanExpression containsCreatorName(String keyword){
-        return schedule.scheduleCreator.userNameFirst.like("%"+keyword+"%");
+    private BooleanExpression containsKeyword(String keyword){
+        return schedule.scheduleTitle.like("%"+keyword+"%")
+                .or(schedule.scheduleContent.like("%"+keyword+"%"))
+                .or(schedule.schedulePlace.like("%"+keyword+"%"))
+                .or(schedule.scheduleCreator.userNameFirst.like("%"+keyword+"%"));
     }
+
 }
