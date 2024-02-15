65318996d82a7b962d3a4f1b4df8624611d781d3
DevYJKwon <DevYJKwon>
2024-02-15 21:22:04 +0900

    fix: redis value typecast

--- I-works_server/src/main/java/com/example/iworks/global/util/JwtProvider.java
+++ I-works_server/src/main/java/com/example/iworks/global/util/JwtProvider.java
@@ -98,8 +98,7 @@ public class JwtProvider {
         if (type.equals("refresh")) {
             System.out.println("create refresh");
            ValueOperations<String, String> stringValueOperations = redisTemplate.opsForValue();
-            System.out.println(claims.get("id"));
-            String redisValue = stringValueOperations.get(claims.get("id"));
+            String redisValue = stringValueOperations.get(String.valueOf(claims.get("id")));
             if (redisValue != null) {
                 return claims.getExpiration().after(new Date());
             }
