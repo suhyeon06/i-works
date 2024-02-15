35da65b3f413488aa604009eabf4bf4db87886d6
이수현 <suhyeon9120@gmail.com>
2024-02-15 18:02:32 +0900

    feat: add AdminTeamService

---
+++ I-works_server/src/main/java/com/example/iworks/domain/admin/service/adminTeam/AdminTeamService.java
@@ -0,0 +1,17 @@
+package com.example.iworks.domain.admin.service.adminTeam;
+
+import com.example.iworks.domain.address.dto.request.AddressTeamEditRequestDto;
+import com.example.iworks.domain.address.dto.request.AddressTeamUserAddRequestDto;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
+public interface AdminTeamService {
+    ResponseEntity<Map<String, Object>> deleteTeam(int teamId);
+
+    ResponseEntity<Map<String, Object>> addTeamUser(int teamId, AddressTeamUserAddRequestDto requestDto);
+
+    ResponseEntity<Map<String, Object>> removeTeamUser(int teamId, int targetId);
+
+    ResponseEntity<Map<String, Object>> editTeam(int teamId, AddressTeamEditRequestDto requestDto);
+}
