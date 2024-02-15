9178f28a4fcfe15a8458b4e8582c83a659ffc8d7
giryun <pkr951113@ajou.ac.kr>
2024-02-16 02:37:50 +0900

    refactor : Modify authorization in board crud api

---
+++ I-works_server/src/main/java/com/example/iworks/global/common/BoardCodeDef.java
@@ -0,0 +1,9 @@
+package com.example.iworks.global.common;
+
+public class BoardCodeDef {
+
+    public static final int BOARD_CATEGORY_ANNOUNCEMENT = 1;
+    public static final int BOARD_CATEGORY_FREE = 2;
+    public static final int BOARD_CATEGORY_DEPARTMENT = 3;
+
+}
